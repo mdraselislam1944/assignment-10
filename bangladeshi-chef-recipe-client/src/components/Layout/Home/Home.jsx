7f2a5a4149e070e20d66f561ae7f33899f84f3f9
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-05-04 12:10:59 +0600

    'add-another-section-file'

--- bangladeshi-chef-recipe-client/src/components/Layout/Home/Home.jsx
+++ bangladeshi-chef-recipe-client/src/components/Layout/Home/Home.jsx
@@ -5,6 +5,8 @@ import { useLoaderData } from 'react-router-dom';
 import ChefCard from '../../ChefCard/ChefCard';
 import Spinner from 'react-bootstrap/Spinner';
 
+import image from '../../../assets/chef3.jpg';
+
 import { Button, Card } from 'react-bootstrap';
 
 const Home = () => {
@@ -46,8 +48,27 @@ const Home = () => {
     </Spinner>
 
   }
-           </div>
-        </div>
+      </div>
+
+      <h1 className='text-center mt-5 mb-3 '>Our Provided service in the Bangladesh</h1>
+      <div className='d-flex my-5'>
+      <div className='my-auto mx-5'>
+      <ul>
+     <h3>
+     Recipe services can be useful for people who are looking for new and interesting recipes to try, as well as for those who are trying to plan their meals and grocery shopping more efficiently. Many recipe services also offer mobile apps, making it easy for users to access recipes and meal planning tools on the go.
+     </h3>
+              <li>Convenience: Recipe services make it easy to search for and find recipes that fit your needs, whether you are looking for a specific ingredient or meal type. You can access a wide range of recipes from your computer or mobile device, making it easy to plan your meals and grocery shopping from anywhere.</li>
+              <li>
+              Variety: Recipe services offer a vast collection of recipes from different cuisines and cultures, so you can explore new flavors and ingredients that you may not have tried before. You can also filter recipes by dietary preferences or restrictions, ensuring that you find recipes that fit your lifestyle and preferences.
+              </li>
+              <li>User-generated content: Many recipe services allow users to rate and review recipes, providing valuable insights and feedback about the recipes taste, difficulty level, and overall appeal. User-generated content can help you choose the best recipes to try and avoid recipes that may not be as successful.</li>
+            </ul>
+      </div> 
+    <div >
+       <img className='rounded'  style={{ width: '36rem',height:'40rem' }} src={image} alt="" /> 
+    </div> 
+      </div>
+      </div>
     );
 };
 
